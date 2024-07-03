1ee2e0ed0a2192340a5d1ed9cd369c0cd92da416
OverLeo007 <sokolov_lev2003@mail.ru>
2024-07-03 15:44:32 +0700

    project init

---
+++ app/src/main/java/ru/hihit/cobuy/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package ru.hihit.cobuy.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightCream = Color(0xFFFFF7EB)
+val LightGray = Color(0xFFEFEAE3)
+val WarmGray = Color(0xFFC7C1B8)
+val DarkTaupe = Color(0xFF807770)
+val DarkGray = Color(0xFF2D2D2D)
+
+val Charcoal = Color(0xFF4B4A48)
+val AshGray = Color(0xFF5A5855)
+val MediumGray = Color(0xFF888178)
+val PaleBrown = Color(0xFFB9ACA1)
+val OffWhite = Color(0xFFF4F4F4)
